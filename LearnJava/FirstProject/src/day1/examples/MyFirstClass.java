0f9642de0cae7580e3b33d33da2bed356e1197d2
Philip Noble <thesuperchimp@googlemail.com>
2020-01-31 16:16:25 +0000

    New tutorial approach

---
+++ LearnJava/FirstProject/src/day1/examples/MyFirstClass.java
@@ -0,0 +1,15 @@
+package day1.examples;
+
+public class MyFirstClass {
+/*
+ * How to
+ * Make a
+ * Multi-line Comment
+ */
+	public static void main(String[] args) {
+		// A normal comment
+		System.out.println("Hello World");
+
+	}
+
+}
