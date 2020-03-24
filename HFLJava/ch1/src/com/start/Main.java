a817872c3a5d5469f19b4fdc067dbe05980c2897
Philip Noble <thesuperchimp@googlemail.com>
2020-03-24 12:33:50 +0000

    fixed a name error

--- HFLJava/ch1/src/com/start/Main.java
+++ HFLJava/ch1/src/com/start/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// Just copy and paste code in here for now
 
-        int[] test = new int[]{1,2,1,4,1,6,1,8,1,10,2,2,2,23,23,2,2,23,2,2,23,2,2,2,2,23,23,23,23,23};
+        int[] test = new int[]{1,2,1,4,1,6,1};
         SumOfOdds go = new SumOfOdds();
         System.out.println("result: " + go.sumOfOdds(test));
 
